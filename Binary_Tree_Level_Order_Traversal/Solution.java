b3a5cd3a2c82244c918223f1ddba0307f501aa18
= <=>
2013-08-01 18:53:58 +0800

    Binary_Tree_Level_Order_Traversal

---
+++ Binary_Tree_Level_Order_Traversal/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        ArrayDeque<TreeNode> q = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> qNext = new ArrayDeque<TreeNode>();
+        q.add(root);
+        while (q.size() > 0) {
+            ArrayList<Integer> cell = new ArrayList<Integer>();
+            while (q.size() > 0) {
+                TreeNode node = q.pollFirst();
+                cell.add(node.val);
+                if (node.left != null) {
+                    qNext.add(node.left);
+                }
+                if (node.right != null) {
+                    qNext.add(node.right);
+                }
+            }
+            result.add(cell);
+            q = qNext;
+            qNext = new ArrayDeque<TreeNode>();
+        }
+        return result;
+    }
+}
