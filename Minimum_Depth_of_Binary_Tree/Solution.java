2e99077961f793399320343fddb85a8c3844acc9
= <=>
2013-08-02 11:53:24 +0800

    Minimum_Depth_of_Binary_Tree

---
+++ Minimum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (root == null) {
+            return 0;
+        } else if (root.left == null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);
+    }
+}
